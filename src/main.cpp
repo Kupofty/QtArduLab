3fd6c8d0c98e28a9849db2c3d087be0af73576f6
Mathis Provost <mattpro83@gmail.com>
2025-12-03 01:44:11 +0100

    Rename project file

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     AppController w;
 
-    w.setWindowTitle("QtSerialLab");
+    w.setWindowTitle("QtArduinoLab");
     w.show();
     return a.exec();
 }
