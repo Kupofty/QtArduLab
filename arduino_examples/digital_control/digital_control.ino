dab2d87b36f24d4511221c127cb0b042472d44b2
Mathis Provost <mattpro83@gmail.com>
2025-12-04 18:49:21 +0100

    add basic ino code example to decypher QtArduLab serial commands

--- arduino_examples/digital_control/digital_control.ino
+++ arduino_examples/digital_control/digital_control.ino
@@ -9,56 +9,57 @@ void setup()
 
 void loop() 
 {
-  if (Serial.available() > 0) 
+  if (!Serial.available() > 0) 
+    return;
+    
+  String message = Serial.readStringUntil('\n');
+  message.trim();
+
+  // Parse message
+  int firstSpace  = message.indexOf(' ');
+  int secondSpace = message.indexOf(' ', firstSpace + 1);
+  if (firstSpace < 0 || secondSpace < 0) 
   {
-    String message = Serial.readStringUntil('\n');
-    message.trim();
-
-    // Parse message
-    int firstSpace  = message.indexOf(' ');
-    int secondSpace = message.indexOf(' ', firstSpace + 1);
-    if (firstSpace < 0 || secondSpace < 0) 
-    {
-      Serial.println("[CMD] Invalid format");
-      return;
-    }
-    
-    // Split into 3 tokens
-    String command  = message.substring(0, firstSpace);
-    String pinStr = message.substring(firstSpace + 1, secondSpace);
-    String value = message.substring(secondSpace + 1);
-
-    //Check pin validity (the string contains only digits)
-    for (int i = 0; i < pinStr.length(); i++) {
-        if (!isDigit(pinStr[i])) {
-          Serial.println("[CMD] Invalid pin");
-          return;
-        }
-    }
+    Serial.println("[CMD] Invalid format");
+    return;
+  }
   
-    int pin = pinStr.toInt(); 
-    
-    // LED CONTROL
-    if (command == "DIGITAL")
+  // Split into 3 tokens
+  String command  = message.substring(0, firstSpace);
+  String pinStr = message.substring(firstSpace + 1, secondSpace);
+  String value = message.substring(secondSpace + 1);
+
+  //Check pin validity (the string contains only digits)
+  for (int i = 0; i < pinStr.length(); i++) {
+      if (!isDigit(pinStr[i])) {
+        Serial.println("[CMD] Invalid pin");
+        return;
+      }
+  }
+
+  int pin = pinStr.toInt(); 
+  
+  // LED CONTROL
+  if (command == "DIGITAL")
+  {
+    if (value == "HIGH")
     {
-      if (value == "HIGH")
-      {
-        digitalWrite(pin, HIGH);
-        Serial.println("[CMD] Success");
-      }
-      else if (value == "LOW")
-      {
-        digitalWrite(pin, LOW);
-        Serial.println("[CMD] Success");
-      }
-      else
-      {
-        Serial.println("[CMD] Invalid value");
-      }
+      digitalWrite(pin, HIGH);
+      Serial.println("[CMD] Success");
+    }
+    else if (value == "LOW")
+    {
+      digitalWrite(pin, LOW);
+      Serial.println("[CMD] Success");
     }
     else
     {
-      Serial.println("[CMD] Invalid command");
+      Serial.println("[CMD] Invalid value");
     }
   }
+  else
+  {
+    Serial.println("[CMD] Invalid command");
+  }
+
 }
--- docs/ArduinoCommandsList.md
+++ docs/ArduinoCommandsList.md
@@ -54,8 +54,9 @@ These messages are displayed in the **Sensors / Logs** text panel along with a t
 Custom prefixes (e.g., `[SENSOR], [LOGS], [WARNING]`,...) can be added to give more info in the QtArduLab text panel.
 
 ------
-## 3. Arduino Validation Rules
+## 3. Arduino Commands Interpretation 
 
+#### 3.1 Validation Rules
 The Arduino sketch needs to validate incoming commands according to the following rules:
 
 1. The incoming message must be stripped  in three tokens.  
@@ -66,3 +67,62 @@ The Arduino sketch needs to validate incoming commands according to the followin
 
 > **Note:** It is the responsibility of the user to correctly implement this validation logic in the Arduino sketch.  
 Ready-to-use examples demonstrating a proper implementation are available in the **`arduino_examples/`** folder.
+
+#### 3.2  Code example
+```
+#define DEVICE_PIN 8
+#define BAUDRATE 9600
+
+void setup() 
+{ 
+  Serial.begin(BAUDRATE); 
+  pinMode(DEVICE_PIN, OUTPUT); 
+}
+
+void loop() 
+{
+  if (!Serial.available() > 0) 
+    return;
+    
+  String message = Serial.readStringUntil('\n');
+  message.trim();
+
+  // Parse message
+  int firstSpace  = message.indexOf(' ');
+  int secondSpace = message.indexOf(' ', firstSpace + 1);
+  if (firstSpace < 0 || secondSpace < 0) 
+  {
+    Serial.println("[CMD] Invalid format");
+    return;
+  }
+  
+  // Split into 3 tokens
+  String command  = message.substring(0, firstSpace);
+  String pinStr = message.substring(firstSpace + 1, secondSpace);
+  String valueStr = message.substring(secondSpace + 1);
+
+  //Check pin validity (the string contains only digits)
+  for (int i = 0; i < pinStr.length(); i++) {
+      if (!isDigit(pinStr[i])) {
+        Serial.println("[CMD] Invalid pin");
+        return;
+      }
+  }
+
+  int pin = pinStr.toInt(); 
+  
+  if (command == "XXXX")
+  {
+    // cast valueStr to int/double if necessary
+    // use if/else, switch, etc on value/valueStr to do actions
+  }
+  else if (command == "YYYY")
+  {
+    //actions
+  }
+  else
+  {
+    Serial.println("[CMD] Invalid command");
+  }
+```
+
